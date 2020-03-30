279a245a00f51219e43145a4be30fc2e1903824b
chanathip2323 <chanathip.ru.60@ubu.ac.th>
2020-03-30 14:42:57 +0700

    work

---
+++ work1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "1", "2", "3", "4","5","6","7","8","9","A" };
+            string[] array0 = new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+            int count = 1;
+            while (count <= 12)
+            {
+                Console.WriteLine("Please choose LED to turn On/Off:");
+                String f = Console.ReadLine();
+                for (int i = 0; i < array.Length; i++){
+                    if (array[i] == f & array0[i] == "[ ]"){
+                        array0[i] = "[!]";
+                    }
+                    else if (array[i] == f & array0[i] == "[!]"){
+                        array0[i] = "[ ]";
+                    }
+
+                }
+                Console.WriteLine(String.Join(" ", array0));
+                Console.WriteLine("1   2   3   4   5   6   7   8   9   A");
+                count ++;
+            }
+        }
+    }
+}

b728537a8ccfb56187fa84e0d8b1c398765f38af
chanathip2323 <chanathip.ru.60@ubu.ac.th>
2020-04-06 12:43:25 +0700

    1

---
+++ homework1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHomework1 Led = new ControlLed();
+            Console.WriteLine(Led.DisplayLEDOnScreen(""));
+            Console.ReadLine();
+        }
+    }
+}
+public interface IHomework1
+{
+    string DisplayLEDOnScreen(string ledNo);
+}
+public class ControlLed : IHomework1
+{
+    public string DisplayLEDOnScreen(string ledNo)
+    {
+        string[] array1 = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
+        string[] array2 = new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+        int count = 1;
+        while (count <= 100)
+        {
+            string DisplayLEDOnScreen;
+            DisplayLEDOnScreen = ledNo;
+            Console.WriteLine(String.Join(" ", array2));
+            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A ");
+            count++;
+            Console.WriteLine("Please choose LED to turn On/Off:");
+            String LED = Console.ReadLine();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] == LED & array2[i] == "[ ]")
+                {
+                    array2[i] = "[!]";
+                }
+                else if (array1[i] == LED & array2[i] == "[!]")
+                {
+                    array2[i] = "[ ]";
+                }
+            }
+        }
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ homework2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System;
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Testhome proall = new Testhome();
+            proall.AddProductToCart();
+            Console.ReadLine();
+        }
+    }
+}
+public interface IHomework09
+{
+    IEnumerable<IProduct> GetAllProducts();
+    void AddProductToCart(IProduct product);
+    IEnumerable<IProduct> GetProductsInCart();
+}
+public interface IProduct
+{
+    string SKU { get; set; }
+    string Name { get; set; }
+    double Price { get; set; }
+}
+public class Testhome : IHomework09
+{
+    public void AddProductToCart()
+    {
+        using (var reader = new StreamReader(@"product.csv"))
+        {
+            List<string> SKU = new List<string>();
+            List<string> Name = new List<string>();
+            List<string> Price = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                SKU.Add(values[0]);
+                Name.Add(values[1]);
+                Price.Add(values[2]);
+            }
+            string[] array1 = new string[10];
+            ArrayList myAL = new ArrayList();
+            int sum = 0;
+            int count = 1;
+            Console.WriteLine("Products in your cart are");
+            Console.WriteLine("none");
+            Console.WriteLine("---");
+            Console.WriteLine("Total amount: 0.00 baht");
+            Console.WriteLine("Please input a product key:");
+            while (count <= 100)
+            {
+                string inputid = Console.ReadLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    if (SKU[i] == inputid)
+                    {
+                        string v = Price[i];
+                        int x = Convert.ToInt32(v);
+                        sum = sum + x;
+                        array1[i - 1] = Name[i];
+                    }
+                }
+                count = count + 1;
+                Console.WriteLine("Products in your cart are");
+                Console.WriteLine("{0}", string.Join(" ", array1));
+                Console.WriteLine("-------");
+                Console.WriteLine("Total amount: " + sum + " Bath");
+                Console.WriteLine("Please input a product key:");
+            }
+        }
+    }
+    public void AddProductToCart(IProduct product)
+    {
+    }
+    public IEnumerable<IProduct> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+    public IEnumerable<IProduct> GetProductsInCart()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Testproduct : IProduct
+{
+    private string _sku;
+    public string SKU
+    {
+        get { return _sku; }
+        set { _sku = value; }
+    }
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    private string _price;
+    public string Price
+    {
+        get { return _price; }
+        set { _price = value; }
+    }
+    double IProduct.Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
+
+
+
